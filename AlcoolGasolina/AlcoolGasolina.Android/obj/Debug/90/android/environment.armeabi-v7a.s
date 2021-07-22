bf1d32c8a0c1cf8359036e839dec4e0edb45df98
ivoosantos <oliveiradossantosivo@gmail.com>
2021-07-22 18:34:16 -0300

    Teste

--- AlcoolGasolina/AlcoolGasolina.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ AlcoolGasolina/AlcoolGasolina.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"d2543e12-2830-4279-90eb-deede0ed9d9e"
+	.asciz	"dd038c31-90bf-4cfc-ab6f-f9bec74a3d5a"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
