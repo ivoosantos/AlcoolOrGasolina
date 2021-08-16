d788330b4dd151864b4098df2175d1d8162c60c2
ivoosantos <oliveiradossantosivo@gmail.com>
2021-08-16 16:27:02 -0300

    Dll, Cache e etc.

--- AlcoolGasolina/AlcoolGasolina.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ AlcoolGasolina/AlcoolGasolina.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"0d78d49c-8a8f-4d49-b5d3-88677bce60f1"
+	.asciz	"d77e8bc8-23df-4ada-99d8-cd0b22dd4a6c"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
