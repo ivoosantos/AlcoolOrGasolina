dea64088577891dcdb7b9659a6b4e35ded99c71a
ivoosantos <oliveiradossantosivo@gmail.com>
2021-08-16 17:44:56 -0300

    Teste

---
+++ AlcoolGasolina/AlcoolGasolina.Android/DependencyServices/GetLocationAndroid.cs
@@ -0,0 +1,41 @@
+﻿using AlcoolGasolina.Droid.DependencyServices;
+using Android.App;
+using Android.Content;
+using Android.Locations;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using App5.DependencyServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AlcoolGasolina.Droid.DependencyServices.Location))]
+namespace AlcoolGasolina.Droid.DependencyServices
+{
+    public class Location : ILocation
+    {
+        [Obsolete]
+        LocationManager LM = (LocationManager)Forms.Context.GetSystemService(Context.LocationService);
+
+        [Obsolete]
+        public bool IsEnabled()
+        {
+            return LM.IsProviderEnabled(LocationManager.GpsProvider);
+        }
+
+        [Obsolete]
+        public async Task OpenSettings()
+        {
+            if (LM.IsProviderEnabled(LocationManager.GpsProvider) == false)
+            {
+                Context ctx = Forms.Context;
+                ctx.StartActivity(new Intent(Android.Provider.Settings.ActionLocationSourceSettings));
+            }
+        }
+    }
+}
