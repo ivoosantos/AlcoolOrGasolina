752d0f4d7d346f9b5ab4a65c087bfe63770d353a
ivoosantos <oliveiradossantosivo@gmail.com>
2021-10-09 13:36:00 -0300

    [BranchIvo]Testando APP

--- AlcoolGasolina/AlcoolGasolina.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ AlcoolGasolina/AlcoolGasolina.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -92,7 +92,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"b313ebf7-1973-4583-a63d-6b5724c33374"
+	.asciz	"bf8a8ab3-e4fe-4dc8-8741-f6324c1f7614"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
